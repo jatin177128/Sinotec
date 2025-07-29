56c9c0d5d19eddb6b7ccf4710f94dce31821acec
jatin177128 <aryajatin177@gmail.com>
2025-07-29 10:44:53 +0530

    contact changes

--- src/components/sections/ContactSection.jsx
+++ src/components/sections/ContactSection.jsx
@@ -28,9 +28,9 @@ const ContactSection = () => {
 
   // EmailJS configuration
  const EMAILJS_CONFIG = {
-  PUBLIC_KEY: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-  SERVICE_ID: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  TEMPLATE_ID: import.meta.env.VITE_EMAILJS_TEMPLATE_ID
+  PUBLIC_KEY: 'zhgIV1CC3d4r4pgxx',
+  SERVICE_ID: 'service_e6yb4wr',
+  TEMPLATE_ID: 'template_hd5l40h'
 };
 
   React.useEffect(() => {
